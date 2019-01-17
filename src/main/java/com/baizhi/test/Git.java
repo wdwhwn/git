e8423b2013c8a85fd7117d0393480c9c8f714a26
wdwhwn <wdwhwn@163.com>
2019-01-17 18:57:37 +0800

    testA

--- src/main/java/com/baizhi/test/Git.java
+++ src/main/java/com/baizhi/test/Git.java
@@ -4,4 +4,7 @@ package com.baizhi.test;
  * Created by wdwhwn on 2019/1/17.
  */
 public class Git {
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAa");
+    }
 }
