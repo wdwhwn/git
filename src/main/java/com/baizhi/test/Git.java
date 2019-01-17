722041237a59759cda9c26d4d17e328aefc3fbd1
wdwhwn <wdwhwn@163.com>
2019-01-17 18:55:52 +0800

    master

--- src/main/java/com/baizhi/test/Git.java
+++ src/main/java/com/baizhi/test/Git.java
@@ -4,4 +4,11 @@ package com.baizhi.test;
  * Created by wdwhwn on 2019/1/17.
  */
 public class Git {
+    public static void main(String[] args) {
+        System.out.println("ABC");
+        System.out.println("bcd");
+        System.out.println("cde");
+        System.out.println("AAAA");
+    }
+
 }
